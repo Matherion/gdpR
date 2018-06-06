72480d3c672466220e9d2e887a2e25e5636f4b87
Gjalt-Jorn Peters <gjalt-jorn@greatergood.eu>
2018-06-06 09:51:25 +0200

    Added privateToPublic

---
+++ R/privateToPublic.R
@@ -0,0 +1,11 @@
+privateToPublic <- function(privateDatafile = NULL,
+                            privateDataPath = NULL,
+                            privateDatafileRegex = NULL,
+                            scriptfile = NULL,
+                            publicDataPath = NULL,
+                            publicDataFileGsub = c('PRIVATE', 'public'),
+                            importLimeSurveyDataOptions = NULL,
+                            anonymizerOptions = NULL,
+                            secret_public_key = NULL) {
+  ### Do stuff;
+}
